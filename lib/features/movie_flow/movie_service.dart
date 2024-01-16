84dc081ad2fd54c5acb5d6fe80b81e194556ee86
quingsley <jeromejuma25@gmail.com>
2024-01-16 13:17:27 +0300

    Add error handling and failure class

--- lib/features/movie_flow/movie_service.dart
+++ lib/features/movie_flow/movie_service.dart
@@ -1,13 +1,15 @@
 import 'dart:math';
 
+import 'package:dartz/dartz.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:movie_app/core/failure.dart';
 import 'package:movie_app/features/movie_flow/genre/genre.dart';
 import 'package:movie_app/features/movie_flow/movie_repository.dart';
 import 'package:movie_app/features/movie_flow/results/movie.dart';
 
 abstract class MovieService {
-  Future<List<Genre>> getGenres();
-  Future<Movie> getRecommendedMovies(
+  Future<Either<Failure, List<Genre>>> getGenres();
+  Future<Either<Failure, Movie>> getRecommendedMovies(
       List<Genre> genres, int rating, int yearsBack,
       [DateTime? yearsBackFromDate]);
 }
@@ -19,27 +21,37 @@ class TMDBMovieService implements MovieService {
   );
 
   @override
-  Future<List<Genre>> getGenres() async {
-    final genres = await _movieRepository.getMovieGenres();
+  Future<Either<Failure, List<Genre>>> getGenres() async {
+    try {
+      final genres = await _movieRepository.getMovieGenres();
 
-    return genres.map((e) => Genre.fromEntity(e)).toList();
+      return right(genres.map((e) => Genre.fromEntity(e)).toList());
+    } on Failure catch (failure) {
+      return left(failure);
+    }
   }
 
   @override
-  Future<Movie> getRecommendedMovies(
+  Future<Either<Failure, Movie>> getRecommendedMovies(
       List<Genre> genres, int rating, int yearsBack,
       [DateTime? yearsBackFromDate]) async {
     final date = yearsBackFromDate ?? DateTime.now();
     final year = date.year - yearsBack;
     final genreIds = genres.map((e) => e.id).toList().join(',');
-    final movies = await _movieRepository.getRecommendedMovies(
-        '$year-01-01', genreIds, rating.toDouble());
+    try {
+      final movies = await _movieRepository.getRecommendedMovies(
+          '$year-01-01', genreIds, rating.toDouble());
 
-    final recommendedMovies =
-        movies.map((e) => Movie.fromEntity(e, genres)).toList();
-    var random = Random();
-    var radomInt = random.nextInt(recommendedMovies.length);
-    return recommendedMovies[radomInt];
+      if (movies.isEmpty) return left(Failure(message: 'No movies found'));
+
+      final recommendedMovies =
+          movies.map((e) => Movie.fromEntity(e, genres)).toList();
+      var random = Random();
+      var radomInt = random.nextInt(recommendedMovies.length);
+      return right(recommendedMovies[radomInt]);
+    } on Failure catch (failure) {
+      return left(failure);
+    }
   }
 }
 
