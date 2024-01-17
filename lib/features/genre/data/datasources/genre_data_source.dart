0491c1ac9a250c22005925da95fa89a6f4b22419
quingsley <jeromejuma25@gmail.com>
2024-01-17 09:08:02 +0300

    Add GenreDataSource implementation

---
+++ lib/features/genre/data/datasources/genre_data_source.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:movie_app/core/env_variables.dart';
+import 'package:movie_app/core/error/failures.dart';
+import 'package:movie_app/features/genre/data/models/genre_model.dart';
+import 'package:movie_app/features/genre/domain/entities/genre_entity.dart';
+
+abstract class GenreDataSource {
+  Future<List<GenreEntity>> getGenres();
+}
+
+class GenreDataSourceImpl extends GenreDataSource {
+  final Dio client;
+
+  GenreDataSourceImpl({required this.client});
+
+  @override
+  Future<List<GenreEntity>> getGenres() async {
+    try {
+      if (apiKey.isEmpty) {
+        throw AssertionError('TMDB_KEY is not set');
+      }
+      var response = await client.get('/genre/movie/list', queryParameters: {
+        'api_key': apiKey,
+        'language': 'en-US',
+      });
+
+      var results = response.data['genres'] as List<dynamic>;
+
+      return results.map((e) => GenreModel.fromJson(e)).toList();
+    } on DioException catch (e) {
+      if (e.error is SocketException) {
+        throw Failure(
+            message: 'No internet connection',
+            exception: e,
+            code: e.response?.statusCode);
+      }
+      throw Failure(
+          message: e.response?.statusMessage ?? 'Something went wrong',
+          exception: e,
+          code: e.response?.statusCode);
+    }
+  }
+}
