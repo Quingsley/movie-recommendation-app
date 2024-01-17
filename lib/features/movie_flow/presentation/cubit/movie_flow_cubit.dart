7b53b4a30a76bafd269070b251183b311ea31a83
quingsley <jeromejuma25@gmail.com>
2024-01-17 15:22:03 +0300

    Refactor MovieFlowCubit to use PageController directly

--- lib/features/movie_flow/presentation/cubit/movie_flow_cubit.dart
+++ lib/features/movie_flow/presentation/cubit/movie_flow_cubit.dart
@@ -1,29 +1,34 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:equatable/equatable.dart';
 import 'package:movie_app/features/genre/presentation/cubit/genre_cubit.dart';
 
-part 'movie_flow_state.dart';
-
-class MovieFlowCubit extends Cubit<MovieFlowState> {
-  MovieFlowCubit() : super(MovieFlowInitial(PageController()));
+class MovieFlowCubit extends Cubit<PageController> {
+  MovieFlowCubit() : super(PageController());
 
   void nextPage({BuildContext? context}) {
-    debugPrint('page: ${state.controller.page}');
-    if (state.controller.page! >= 1) {
+    debugPrint('current page: ${state.page}');
+    final nextPage = state.page! + 1;
+    debugPrint('next page: $nextPage');
+    if (state.page == 1) {
       if (context != null) {
-        // check if genre list is empty and return
-        if (context.read<GenreCubit>().state.genres.isEmpty) return;
+        // check if any genre is selected and if none return
+        final condition = context
+            .read<GenreCubit>()
+            .state
+            .genres
+            .any((genre) => genre.isSelected);
+        if (!condition) return;
       }
     }
-    state.controller.nextPage(
+
+    state.nextPage(
       duration: const Duration(milliseconds: 600),
       curve: Curves.easeInOut,
     );
   }
 
   void previousPage() {
-    state.controller.previousPage(
+    state.previousPage(
       duration: const Duration(milliseconds: 600),
       curve: Curves.easeInOut,
     );
@@ -31,7 +36,7 @@ class MovieFlowCubit extends Cubit<MovieFlowState> {
 
   @override
   Future<void> close() {
-    state.controller.dispose();
+    state.dispose();
     return super.close();
   }
 }
