96edcadcb30ecc0690987eeaa019fe988f4c5a54
quingsley <jeromejuma25@gmail.com>
2024-01-18 14:20:16 +0000

    Add DioProvider class

---
+++ lib/dio_provider.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+
+@Injectable(as: Dio)
+@module
+abstract class DioProvider {
+  @singleton
+  Dio dio() => Dio(
+        BaseOptions(
+          baseUrl: 'https://api.themoviedb.org/3',
+          connectTimeout: const Duration(seconds: 10),
+        ),
+      );
+}
