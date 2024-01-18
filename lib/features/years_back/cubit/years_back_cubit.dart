a0d9219d83b392e3660b428b27e8736d34b6aae3
quingsley <jeromejuma25@gmail.com>
2024-01-18 14:22:14 +0000

    Add injectable annotation to YearsBackCubit

--- lib/features/years_back/cubit/years_back_cubit.dart
+++ lib/features/years_back/cubit/years_back_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
 
+@Injectable()
 class YearsBackCubit extends Cubit<int> {
   YearsBackCubit() : super(10);
 
