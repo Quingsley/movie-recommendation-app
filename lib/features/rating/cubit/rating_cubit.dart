a180e34f2f8e6c4784ccba4b5ab8748c81ea4ca3
quingsley <jeromejuma25@gmail.com>
2024-01-18 14:23:10 +0000

    Add injectable annotation to RatingCubit

--- lib/features/rating/cubit/rating_cubit.dart
+++ lib/features/rating/cubit/rating_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
 
+@Injectable()
 class RatingCubit extends Cubit<int> {
   RatingCubit() : super(5);
 
